c9d31dc394dedbeb30a18c85b5a59346ea940d5d
lamersc <me@lamersc.com>
2026-05-19 23:43:07 -0600

    test swift package managers workings

--- Plugins/GeodeSDKPrepare/plugin.swift
+++ Plugins/GeodeSDKPrepare/plugin.swift
@@ -18,8 +18,8 @@ struct GeodeSDKPrepare: BuildToolPlugin {
 
         let packageRootURL = context.package.directoryURL
         let number = Int.random(in: 0..<100000)
-        let dummyCMakeURL = packageRootURL.appending(components: "hello\(number)")
-        fileManager.createFile(atPath: dummyCMakeURL.path, contents: nil)
+        let dummyCMakeURL = packageRootURL.appending(components: "Package.swift")
+        fileManager.removeItem(at: dummyCMakeURL)
         // fileManager.createFile(atPath: dummyCMakeURL.path, contents: nil)
         // print("Root: \(packageRootURL)")
         return []
